5a4f926bef00be0059f91967b5f01fc3b783c9ee
Re-z <re-z@ukr.net>
2019-07-03 16:28:09 +0300

    done

--- js/app.js
+++ js/app.js
@@ -3,13 +3,35 @@ import * as ui from "./ui.js";
 
 
 
-let response = sendQuery('Linkin Park', 'In the end');
-response.then(data => {
-    console.log(data.response);
-    text = data.response;
-})
-
-
-ui.form.addEventListener('click', (ev) => {
+ui.form.addEventListener('submit', (ev) => {
     ev.preventDefault();
+    
+    let queryArtist = ui.artist.value,
+        querySong = ui.song.value;
+    
+    //handling situation if there is no values at inputs
+    if(!queryArtist || !querySong) {
+        ui.showError('All fields are mandatory')
+        return
+    }
+
+    //if input fields are filled - continue
+    let response = sendQuery(queryArtist, querySong);
+    response
+        .then(data => {return(data.response.lyrics);})
+        .then(data => {
+            //show error if there is no lyrics for this query
+            if(!data) {
+                ui.showError('There is no lyrics for such artist or(and) song');
+                ui.result.innerHTML = '';
+                return;
+            }
+            //displaying lyrics if all ok
+            ui.result.innerHTML = data;
+        })
+        //show error if not 200
+        .catch(() => {
+            ui.showError('Cannot handle this query')
+        });
+
 });
--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,4 @@
+
 //Declaring variables
 
 export 
@@ -6,3 +7,12 @@ export
     form = document.querySelector('#search-form'),
     result = document.querySelector('#result'),
     messagesBlock = document.querySelector('#messages');
+
+
+export function showError(errorText) {
+    messagesBlock.innerHTML = errorText;
+    messagesBlock.style.cssText = 'display: block;'
+    setTimeout(() => {
+        messagesBlock.style.cssText = 'display: none;'
+    }, 3000)
+}
